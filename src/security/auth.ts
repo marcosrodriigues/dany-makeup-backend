d8a6a90abbe1e993673d1581fc3ec3fe586f95ce
Marcos Rodrigues <marcos.rodriiigues@gmail.com>
2020-06-08 20:40:28 -0300

    Hello world, server!

---
+++ src/security/auth.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+
+module.exports = async (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).send({ error: 'No token provided! '});
+    }
+
+    const [scheme, token] = authHeader.split(' ');
+
+    try {
+        const decoded = await promisify(jwt.verify)(token, "dany_makeup_api");
+
+        req.userId = decoded.id;
+
+        return next();
+    } catch (err) {
+        return res.status(401).json({ error: 'Token invalid'});
+    }
+}
